75d087a3b94cd53dd9017f828f5374be7ef0aefc
louis baptiste <louisvis@ymail.com>
2018-03-16 07:01:34 +0000

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,12 @@
 def looping
-	#your code here
+  counter = 0
+	loop do
+    counter +=1
+    puts "Iteration #{counter} of the loop"
+    if counter >= 10 # If our counter is 10 or more
+      break # Stop the loop
+    end
+  end
 end
 
-#call your method here
+looping
